402467c47e6cc2337878052e27af0dcb2585ae00
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-02-15 08:50:18 +0900

    JdbcTemplate 적용

--- src/main/java/com/devjaewoo/hellospring/SpringConfig.java
+++ src/main/java/com/devjaewoo/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.devjaewoo.hellospring;
 
+import com.devjaewoo.hellospring.repository.JdbcTemplateMemberRepository;
 import com.devjaewoo.hellospring.repository.MemberRepository;
 import com.devjaewoo.hellospring.repository.MemoryMemberRepository;
 import com.devjaewoo.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
